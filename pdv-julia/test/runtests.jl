b70e627156c100c893d38ed0445c0798d5ad0508
Matthew Pharr <m.pharr@protonmail.com>
2026-03-29 01:39:44 -0400

    remove all pdv-julia. This should be redesigned from the ground up once additional important changes/features are implemented on the python side, which is the most important functionality.

--- pdv-julia/test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using Test
-using PDVKernel
-
-include("test_tree.jl")
-include("test_serialization.jl")
-include("test_environment.jl")
-include("test_namespace.jl")
-include("test_handlers.jl")
-include("test_comms.jl")
